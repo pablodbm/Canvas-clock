84d2a5c1aa53b6a5439833856f691c8aec2d6d2d
pablodbm <65422439+pablodbm@users.noreply.github.com>
2021-02-17 17:40:26 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var c = document.getElementById("idek");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(250, 250, 100, 0, 2 * Math.PI);
+ctx.stroke();
+
+var x = document.getElementById("idek");
+var line = x.getContext("2d")
+line.moveTo(250,250)
+line.beginPath()
+line.
